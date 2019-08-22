3aeac21d8a620ac76af4eaf771fe75c81267a4ba
James Grantham <james.r.grantham@gmail.com>
2019-08-22 17:56:52 +0100

    Refactor and tidy up

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -3,10 +3,14 @@
 // ------------- Person -------------
 
 class Person {
-    constructor (name, age, location) {
-        this.name = name;
-        this.age = age;
-        this.location = location;
+    // constructor (name, age, location) {
+    //     this.name = name;
+    //     this.age = age;
+    //     this.location = location;
+    constructor (data) {
+        this.name = data.name;
+        this.age = data.age;
+        this.location = data.location;
     }
     speak() {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
@@ -14,22 +18,35 @@ class Person {
     }
 }
 
-let testPerson = new Person ('Amy', 5, 'Hull');
+// let testPerson = new Person ('testPerson', 5, 'Imagination Land');
+let testPerson = new Person ({
+    name: 'testPerson',
+    age: 5,
+    location: 'Imagination Land'
+});
 testPerson.speak();
 
 // ------------- Student -------------
 
 class Student extends Person {
-    constructor (name, age, location, previousBackground, className, favSubjects, grade) {
-        super (name, age, location);
-        this.previousBackground = previousBackground;
-        this.className = className;
-        this.favSubjects = favSubjects;
-        this.grade = grade;
+    // constructor (name, age, location, previousBackground, className, favSubjects) {
+    //     super (name, age, location);
+    //     this.previousBackground = previousBackground;
+    //     this.className = className;
+    //     this.favSubjects = favSubjects;
+    //     this.grade = 0;
+    // }
+    constructor (data) {
+        super (data);
+        this.previousBackground = data.previousBackground;
+        this.className = data.className;
+        this.favSubjects = data.favSubjects;
+        this.grade = 0;
     }
     listsSubjects() {
+        const name = this.name;
         this.favSubjects.forEach(function(subject) {
-            console.log(subject);
+            console.log(`${name} is studing ${subject}`);
         })
         return this.favSubjects;
     }
@@ -43,14 +60,24 @@ class Student extends Person {
     }
     graduate() {
         if (this.grade >= 70) {
-            console.log(`${this.name} passes with flying colours!`)
+            console.log(`${this.grade}: ${this.name} passes with flying colours!`);
+            return `${this.grade}: ${this.name} passes with flying colours!`;
         } else {
-            console.log(`${this.name} must try harder!`)
+            console.log(`${this.grade}: ${this.name} does not graduate - must try harder!`);
+            return `${this.grade}: ${this.name} does not graduate - must try harder!`;
         }
     }
 }
 
-let james = new Student ('James', 40, 'Aberdeen', 'Oil and Gas', 'WEBEU3', ['maths', 'physics', 'JS']);
+// let james = new Student ('James', 40, 'Aberdeen', 'Oil and Gas', 'WEBEU3', ['maths', 'physics', 'JS']);
+let james = new Student ({
+    name: 'James',
+    age: 40,
+    location: 'Aberdeen',
+    previousBackground: 'Oil and Gas',
+    className: 'WEBEU3',
+    favSubjects: ['maths', 'physics', 'JS']
+});
 james.speak();
 james.listsSubjects();
 james.PRAssignments('test1');
@@ -59,11 +86,17 @@ james.sprintChallenge('test2');
 // ------------- Instructor -------------
 
 class Instructor extends Person {
-    constructor (name, age, location, speciality, favLanguage, catchphrase) {
-        super (name, age, location);
-        this.speciality = speciality;
-        this.favLanguage = favLanguage;
-        this.catchphrase = catchphrase;
+    // constructor (name, age, location, speciality, favLanguage, catchphrase) {
+    //     super (name, age, location);
+    //     this.speciality = speciality;
+    //     this.favLanguage = favLanguage;
+    //     this.catchphrase = catchphrase;
+    // }
+    constructor (data) {
+        super (data);
+        this.speciality = data.speciality;
+        this.favLanguage = data.favLanguage;
+        this.catchphrase = data.catchphrase;
     }
     demo(subject) {
         console.log(`Today we are learning about ${subject}'.`);
@@ -74,30 +107,57 @@ class Instructor extends Person {
         return `${student.name} receives a perfect score on ${subject}`;
     }
     gradeStudent(student) {
-        return student.grade = Math.floor(Math.random() * 100)
+        return student.grade = Math.floor(Math.random() * 100);
     }
 }
 
-let isaac = new Instructor ('Isaac', 55, 'Ireland', 'cooking', 'JS!', 'one, two, TREE');
+// let isaac = new Instructor ('Isaac', 55, 'Ireland', 'cooking', 'JS!', 'one, two, TREE');
+let isaac = new Instructor ({
+    name: 'Isaac',
+    age: 55,
+    location: 'Ireland',
+    speciality: 'cooking',
+    favLanguage: 'JS!',
+    catchphrase: 'one, two, TREE'
+});
+
 isaac.speak();
 isaac.demo('frying');
 isaac.grade(james, 'JS');
+console.log(`${isaac.name} says "${isaac.catchphrase}"`)
 
-// ------------- Project Manager -------------
+isaac.gradeStudent(james); // refresh page to regenerate
+james.graduate();
+
+// ------------- Project Manager ------------- 
+
+// refactored into object (data) without saving original (enough examples)
 
 class ProjectManager extends Instructor {
-    constructor (name, age, location, speciality, favLanguage, catchphrase, gradClassName, favInstructor) {
-        super (name, age, location, speciality, favLanguage, catchphrase);
-        this.gradClassName = gradClassName;
-        this.favInstructor = favInstructor;
+    constructor (data) {
+        super (data);
+        this.gradClassName = data.gradClassName;
+        this.favInstructor = data.favInstructor;
     }
     standUp(channel) {
-        console.log()
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
         return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
     }
     debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
         return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
 
-let isaacsBoss = new ProjectManager ('Tony', 16, 'London', 'dancing', 'French', 'Whoop whoop', 'WEBEU3', 'Isaac');
+let isaacsBoss = new ProjectManager ({
+    name: 'Tony',
+    age: 16,
+    location: 'London',
+    speciality: 'dancing',
+    favLanguage: 'French',
+    catchphrase: 'Whoop whoop',
+    gradClassName: 'WEBEU3',
+    favInstructor: 'Isaac',
+});
+isaacsBoss.standUp('WEBEU3');
+isaacsBoss.debugsCode(james, 'JS');
