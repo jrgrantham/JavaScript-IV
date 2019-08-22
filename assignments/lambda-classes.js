1b4920dea55be2160c9945c16d4a9d58bf3d6034
James Grantham <james.r.grantham@gmail.com>
2019-08-22 15:57:13 +0100

    Create random grade

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,7 @@
 // CODE here for your Lambda Classes
 
+// ------------- Person -------------
+
 class Person {
     constructor (name, age, location) {
         this.name = name;
@@ -12,25 +14,10 @@ class Person {
     }
 }
 
-class Instructor extends Person {
-    constructor (name, age, location, speciality, favLanguage, catchphrase) {
-        super (name, age, location);
-        this.speciality = speciality;
-        this.favLanguage = favLanguage;
-        this.catchphrase = catchphrase;
-    }
-    demo(subject) {
-        console.log(`Today we are learning about ${subject}'.`);
-        return `Today we are learning about ${subject}'.`;
-    }
-    grade(student, subject) {
-        console.log(`${student.name} receives a perfect score on ${subject}`);
-        return `${student.name} receives a perfect score on ${subject}`;
-    }
-    gradeStudent(student) {
-        return student.grade = Math.floor(Math.random() * 100)
-    }
-}
+let testPerson = new Person ('Amy', 5, 'Hull');
+testPerson.speak();
+
+// ------------- Student -------------
 
 class Student extends Person {
     constructor (name, age, location, previousBackground, className, favSubjects, grade) {
@@ -54,7 +41,49 @@ class Student extends Person {
         console.log(`${this.name} has begun sprint challenge on ${subject}`);
         return `${this.name} has begun sprint challenge on ${subject}`;
     }
+    graduate() {
+        if (this.grade >= 70) {
+            console.log(`${this.name} passes with flying colours!`)
+        } else {
+            console.log(`${this.name} must try harder!`)
+        }
+    }
 }
+
+let james = new Student ('James', 40, 'Aberdeen', 'Oil and Gas', 'WEBEU3', ['maths', 'physics', 'JS']);
+james.speak();
+james.listsSubjects();
+james.PRAssignments('test1');
+james.sprintChallenge('test2');
+
+// ------------- Instructor -------------
+
+class Instructor extends Person {
+    constructor (name, age, location, speciality, favLanguage, catchphrase) {
+        super (name, age, location);
+        this.speciality = speciality;
+        this.favLanguage = favLanguage;
+        this.catchphrase = catchphrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}'.`);
+        return `Today we are learning about ${subject}'.`;
+    }
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+    gradeStudent(student) {
+        return student.grade = Math.floor(Math.random() * 100)
+    }
+}
+
+let isaac = new Instructor ('Isaac', 55, 'Ireland', 'cooking', 'JS!', 'one, two, TREE');
+isaac.speak();
+isaac.demo('frying');
+isaac.grade(james, 'JS');
+
+// ------------- Project Manager -------------
 
 class ProjectManager extends Instructor {
     constructor (name, age, location, speciality, favLanguage, catchphrase, gradClassName, favInstructor) {
@@ -71,18 +100,4 @@ class ProjectManager extends Instructor {
     }
 }
 
-
-
-let james = new Student ('James', 40, 'Aberdeen', 'Oil and Gas', 'WEBEU3', ['maths', 'physics', 'JS']);
-let isaac = new Instructor ('Isaac', 55, 'Ireland', 'cooking', 'JS!', 'one, two, TREE');
 let isaacsBoss = new ProjectManager ('Tony', 16, 'London', 'dancing', 'French', 'Whoop whoop', 'WEBEU3', 'Isaac');
-
-james.speak();
-james.listsSubjects();
-james.PRAssignments('test1');
-james.sprintChallenge('test2');
-
-isaac.speak();
-isaac.demo('frying');
-isaac.grade(james, 'JS');
-
